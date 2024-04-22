6f1be84cab26c6131e715d7f81c69048409826d4
許毓庭 <b10705035@ntu.edu.tw>
2024-04-22 16:16:15 +0800

    Create by Xcode

---
+++ RoomieApp/RoomieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomieAppApp.swift
+//  RoomieApp
+//
+//  Created by Yuting Hsu on 2024/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RoomieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
