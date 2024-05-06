1d4385fcb798e5e6df8d0465465a5486db8682cc
Asuka Matsuura <b10705039@ntu.edu.tw>
2024-05-06 15:22:59 +0800

    Connect to Firestore DB

---
+++ RoomieApp/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  RoomieApp
+//
+//  Created by 松浦明日香 on 2024/05/04.
+//
+
+// TEST FILE
+
+import Foundation
+
+struct Todo: Identifiable, Hashable{
+    var id: String
+    var name: String
+    var notes: String
+}
